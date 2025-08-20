b7f111a2ee340f4e5cd46f63a3ece44f390728a3
Rahim <rahimssenabulya82@gmail.com>
2025-08-20 11:48:22 +0300

    Fix changes

--- src/app/ImageScroll/ImageScroll.tsx
+++ src/app/ImageScroll/ImageScroll.tsx
@@ -126,15 +126,15 @@ ImageScroll() {
 
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '30px'}}>
       <ul>
-      <li><a href="malito: info@zannyaafricaofoundation.org">info@zannyaafricaofoundation.org</a></li>
-      <li><a href="malito: support@zannyaafricaofoundation.org">support@zannyaafricaofoundation.org</a></li>
+      <li><a href="malito: info@zannyaafricafoundation.org">info@zannyaafricafoundation.org</a></li>
+      <li><a href="malito: support@zannyaafricafoundation.org">support@zannyaafricafoundation.org</a></li>
       </ul>
     </div>
 
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '10px', marginTop: '10px'}}>
       <ul>
-      <li><a href="malito: contact@zannyaafricaofoundation.org">contact@zannyaafricaofoundation.org</a></li>
-      <li><a href="malito: admin@zannyaafricaofoundation.org">admin@zannyaafricaofoundation.org</a></li>
+      <li><a href="malito: contact@zannyaafricafoundation.org">contact@zannyaafricafoundation.org</a></li>
+      <li><a href="malito: admin@zannyaafricafoundation.org">admin@zannyaafricafoundation.org</a></li>
       </ul>
     </div>
     </div>
