2bae9e6d08a44ece00ef758bd28fe15b92d40d72
Rahim <rahimssenabulya82@gmail.com>
2025-08-25 11:03:25 +0300

    Fix changes

--- src/app/Programs/page.tsx
+++ src/app/Programs/page.tsx
@@ -27,7 +27,7 @@ Programs(){
     setVisible(true), 100);}, []);
     return(
         <div>
-            <div style={{justifyItems: 'center', gap: '1%'}}>
+            <div style={{justifyItems: 'center', gap: '1%', overflow: 'hidden'}}>
             <StickyBar>
             <FaHome style={{width: '25%', height: '25%'}} color="black" cursor='pointer' onClick={() => router.push('/')} >
             Home</FaHome>
@@ -54,9 +54,9 @@ Programs(){
             
             <div style={{width: '100%', height: '50%',justifyItems: 'center',gap: '1%', margin: '0%',display: 'flex',flexDirection: 'row',
                 padding: '1%', paddingBottom: '0%'}}>
-                <Image src='/images/pic3.jpg' alt="pic" style={{display: 'block', height: '100%', width: '32%'}}></Image>
-                <Image src='/images/pic1.jpg' alt="pic" style={{display: 'block', height: '100%', width: '32%'}}></Image>
-                <Image src='/images/pic4.jpg' alt="pic" style={{display: 'block', height: '100%', width: '32%'}}></Image>
+                <Image src='/images/pic3.jpg' alt="pic" style={{display: 'block', height: '100%', width: '33%'}}></Image>
+                <Image src='/images/pic1.jpg' alt="pic" style={{display: 'block', height: '100%', width: '33%'}}></Image>
+                <Image src='/images/pic4.jpg' alt="pic" style={{display: 'block', height: '100%', width: '33%'}}></Image>
             </div>
 
             <div style={{height: '40%', width: '100%', justifyItems: 'center', backgroundColor: 'white'}}>
