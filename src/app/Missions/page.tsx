05d9fd0698ae0f36b2feabec6ad426c017a9eb38
Rahim <rahimssenabulya82@gmail.com>
2025-08-29 16:28:50 +0300

    Fix changes

--- src/app/Missions/page.tsx
+++ src/app/Missions/page.tsx
@@ -92,7 +92,7 @@ Missions() {
                 <div className={styles.pro}>
                 <p style={{fontWeight: 'bold', fontSize: '30px', marginLeft: '5%',}}>ZANNYA</p>
                 <div style={{width: '100%'}}>
-                <p style={{fontWeight: 'bold',  fontSize: "20px", marginLeft: '5%',lineHeight: '55%'}}>AFRICA FOUNDATION (ZAF)</p>
+                <p style={{fontWeight: 'bold',  fontSize: "25px", marginLeft: '5%',lineHeight: '55%'}}>AFRICA FOUNDATION (ZAF)</p>
                 </div>
                 <p style={{fontSize: '15px', marginLeft: '5%', marginBottom: '5%'}}>PROFILE</p>
                 </div>
