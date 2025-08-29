943e9cfceae88ecde247bc757e07a25aa439d070
Rahim <rahimssenabulya82@gmail.com>
2025-08-29 19:02:12 +0300

    Fix changes

--- src/app/Missions/page.tsx
+++ src/app/Missions/page.tsx
@@ -193,8 +193,8 @@ Missions() {
       <h2 className={styles.title}>Our Leaders</h2>
       <div className={styles.grid}>
         {leaders.map((leader) => (
-          <div key={leader.id} style={{display: 'flex', flexDirection: 'column', gap:'2%', justifyContent: 'center'}}>
-          <div style={{ height: '100%', width: '50%',borderRadius: '1px' }}>
+          <div key={leader.id} style={{display: 'flex', flexDirection: 'column', gap:'2%', justifyItems: 'center'}}>
+          <div style={{ height: '100%', width: '50%',borderRadius: '1%' }}>
             {leader.img && (<Image src={leader.img} alt={leader.name} className={styles.image} loading="lazy" />)}
           </div>
           <div key={leader.id} className={styles.card}>
