2b76e114ada22569097f7bc79003ce07e8e97dae
Rahim <rahimssenabulya82@gmail.com>
2025-09-03 17:02:54 +0300

    Fix can

--- src/app/Newsp/page.tsx
+++ src/app/Newsp/page.tsx
@@ -86,7 +86,7 @@ export default function Newsp() {
       </div>
 
       <div className={styles.imageH}>
-        <Image src="https://res.cloudinary.com/dpwuym7xg/image/upload/v1756827150/zannya/uploads/huanavmdp4e1fksdwde1.jpg" alt="image" style={{ width: '100%', height: '600px', display: 'block' }}></Image>
+        <Image src="https://res.cloudinary.com/dpwuym7xg/image/upload/v1756827150/zannya/uploads/huanavmdp4e1fksdwde1.jpg" alt="image" style={{ width: '100%', height: 'auto', display: 'block' }}></Image>
       </div>
 
       <div style={{ height: '0px', width: '100%', justifyItems: 'center', backgroundColor: '#1e3c72' }}>
--- src/app/Resourcess/page.tsx
+++ src/app/Resourcess/page.tsx
@@ -76,7 +76,7 @@ export default function Resourcess() {
       </div>
 
       <div className={styles.imageH}>
-        <Image src="https://res.cloudinary.com/dpwuym7xg/image/upload/v1756829071/zannya/uploads/kngkholnlp6wvmzq4pa8.jpg" alt="image" style={{ width: '100%', height: '600px', display: 'block' }}></Image>
+        <Image src="https://res.cloudinary.com/dpwuym7xg/image/upload/v1756829071/zannya/uploads/kngkholnlp6wvmzq4pa8.jpg" alt="image" style={{ width: '100%', height: 'auto', display: 'block' }}></Image>
       </div>
 
       <div style={{ padding: '40px', maxWidth: '1000px', margin: 'auto' }}>
