85e657280ebe418ae7c15596d48856ba02f09633
Rahim <rahimssenabulya82@gmail.com>
2025-09-07 12:18:11 +0300

    Fix changes

--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -10,14 +10,12 @@ export async function POST(req: Request) {
 
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+      service: "gmail", // you can also configure SMTP manually
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
     await transporter.sendMail({
       from: `"Zannya Careers" <${process.env.EMAIL_USER}>`,
