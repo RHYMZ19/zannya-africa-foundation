4f96d3e09ab733c9f6c9256f147e7b7b4b8d4fe8
Rahim <rahimssenabulya82@gmail.com>
2025-09-07 10:02:42 +0300

    Fix changes

--- src/app/api/sendContactMail/route.ts
+++ src/app/api/sendContactMail/route.ts
@@ -10,12 +10,14 @@ export async function POST(req: Request) {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // use App Password if Gmail
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: `"Zannya Contact" <${process.env.EMAIL_USER}>`,
--- src/app/api/sendMail/route.ts
+++ src/app/api/sendMail/route.ts
@@ -10,12 +10,14 @@ export async function POST(req: Request) {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail", // you can also configure SMTP manually
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: `"Zannya Careers" <${process.env.EMAIL_USER}>`,
