ffe77b464337ad85a1dd45e6beb288426d09ff32
Rahim <rahimssenabulya82@gmail.com>
2025-09-22 15:06:50 +0300

    Fix changes

--- src/app/Newsp/page.tsx
+++ src/app/Newsp/page.tsx
@@ -8,9 +8,10 @@ import IncreaseIma from "./components/IncreaseIma";
 import ContactUs from "../ContactUs/page";
 import OptionalFeatures from "../OptionalFeatures/OptionalFeatures";
 import styles from './Newsp.module.css';
-import { FaHome, FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import { FaFacebook, FaHome, FaInstagram, FaTiktok } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default async function NewspPage() {
   const news: NewsItem[] = await fetchNews(); // SSR fetch
@@ -48,27 +49,29 @@ export default async function NewspPage() {
 
       <div className={styles.latestnews}>
   <h2>Latest News</h2>
-  {news.slice(0, 5).map(item => (
-    <div key={item.id} className={styles.newsitem}>
-      {item.type && (
-        <span className={`${styles.newstype} ${item.type.toLowerCase()}`}>
-          {item.type}
-        </span>
-      )}
-      <h3 className={styles.newstitle}>{item.title}</h3>
-      <p className={styles.newsdescription}>{item.description}</p>
-      {item.timestamp && (
-        <small className={styles.newstimestamp}>
-          {new Date(item.timestamp).toLocaleDateString('en-UG', {
-            weekday: 'short',
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-          })}
-        </small>
-      )}
-    </div>
-  ))}
+  <div className={styles.newslist}>
+    {news.slice(0, 5).map(item => (
+      <div key={item.id} className={styles.newsitem}>
+        {item.type && (
+          <span className={`${styles.newstype} ${item.type.toLowerCase()}`}>
+            {item.type}
+          </span>
+        )}
+        <h3 className={styles.newstitle}>{item.title}</h3>
+        <p className={styles.newsdescription}>{item.description}</p>
+        {item.timestamp && (
+          <small className={styles.newstimestamp}>
+            {new Date(item.timestamp).toLocaleDateString('en-UG', {
+              weekday: 'short',
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric',
+            })}
+          </small>
+        )}
+      </div>
+    ))}
+  </div>
 </div>
 
       {/* Social links */}
@@ -79,6 +82,7 @@ export default async function NewspPage() {
         <a href="https://facebook.com/zannyaafricafoundation" target="_blank" rel="noopener noreferrer" style={{ color: "blue" }}><FaFacebook /></a>
         <a href="https://instagram.com/zannya_africa_foundation" target="_blank" rel="noopener noreferrer" style={{ color: "pink" }}><FaInstagram /></a>
         <a href="https://tiktok.com/@zannyaafricafdn" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}><FaTiktok /></a>
+        <a href="https://x.com/zannyaafrica" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}><FaXTwitter /> </a>
       </div>
 
       <p style={{ textDecoration: 'underline', color: 'rgb(235, 125, 125)', textAlign: 'center' }}><strong>Or you can email us for:</strong></p>
