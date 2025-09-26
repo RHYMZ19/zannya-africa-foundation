81ec7cc50c16dc87da75e745ccc81be90465c922
Rahim <rahimssenabulya82@gmail.com>
2025-09-26 12:59:05 +0300

    Fix changes

--- src/app/Donate1/Donate1.tsx
+++ src/app/Donate1/Donate1.tsx
@@ -3,7 +3,6 @@
 
 import styles from './Donate1.module.css';
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Donate() {
   
@@ -18,13 +17,7 @@ export default function Donate() {
       />
       <div className={styles.cardContent}>
         <h3 className={styles.headings}>Support Us</h3>
-        <p>54 youth attained certificates in refereeing futsal, this was in collaboration with futsal association of Uganda and FUFA in Kabowa High school Lubaga Division</p>
         <p>Increased awareness on HIV prevention, 90% increase in reproductive health knowledge among 1,000 youth in Bwaise, Ndeeba, Kajjansi and rural outreaches in katakwi and Buyende .</p>
-        <p>
-          Together, we can transform lives. By donating today, you become part of a movement that uplifts communities...
-        </p>
-        <p>Click in the Link below</p>
-        <Link href="/Donates" className={styles.arrowButton}>Read More</Link>
       </div>
     </div>
   );
