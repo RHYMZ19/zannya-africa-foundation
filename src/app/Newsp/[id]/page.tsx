df3f02158e28f2a8620c7da7ce206be2fa5e5b0b
Rahim <rahimssenabulya82@gmail.com>
2025-11-01 13:24:38 +0300

    Fix changes

--- src/app/Newsp/[id]/page.tsx
+++ src/app/Newsp/[id]/page.tsx
@@ -2,9 +2,9 @@ import NewsDetailsPageClient from "./NewsDetailsPageClient";
 
 interface PageProps {
   params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
+  // searchParams?: { [key: string]: string | string[] | undefined }; // remove this
 }
 
-export default async function Page({ params, searchParams }: PageProps) {
+export default async function Page({ params }: PageProps) {
   return <NewsDetailsPageClient id={params.id} />;
 }
