fbbb7a07dc60e04cfd4360b3cd2b344f5ed384f2
Rahim <rahimssenabulya82@gmail.com>
2025-11-25 18:02:40 +0300

    Fix changes

--- src/app/components/WeeklyNewsletter.tsx
+++ src/app/components/WeeklyNewsletter.tsx
@@ -64,7 +64,7 @@ export default function WeeklyNewsletter() {
   if (loading) return <p>Loading newsletters...</p>;
 
   return (
-    <div style={{ display: "flex",justifyContent: "center",}}>
+    <div className={styles.newsletterWrapper}>
   {/* Cards container */}
   <div className={styles.simpleNewsletterList}>
     {newsletters.length === 0 && <p>No newsletters yet.</p>}
