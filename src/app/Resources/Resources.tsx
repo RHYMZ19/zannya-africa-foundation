fcc8f10cb623ef9a512686cbd8e47dc4873fea7a
Rahim <rahimssenabulya82@gmail.com>
2025-12-30 14:54:49 +0300

    Fix dd

--- src/app/Resources/Resources.tsx
+++ src/app/Resources/Resources.tsx
@@ -21,7 +21,7 @@ export default function Resources() {
                 <h3 className={styles.heading}>Resources</h3>
                 <p className={styles.text}>REPORTS (ZAF) </p>
                 <p className={styles.text}>The Report shows Program Highlights Financial breakdown and others</p>
-
+                
                 <p>Download PDF</p>
                 <p className={styles.text}>
                     Explore more of our resources, including reports, publications, research papers... Click in the link below.
