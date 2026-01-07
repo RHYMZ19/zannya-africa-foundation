b1307cc4bd6cbd34d3fa01f4520e4e8e1e1a9386
Rahim <rahimssenabulya82@gmail.com>
2026-01-07 15:23:51 +0300

    Fix cfr

---
+++ src/app/shop/ShopPageClient.tsx
@@ -0,0 +1,8 @@
+// app/shop/ShopPageClient.tsx
+"use client";
+
+import ShopClient from "../products/ShopClient";
+
+export default function ShopPageClient() {
+  return <ShopClient />;
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,6 @@
-"use client";
+// app/shop/page.tsx
+import ShopPageClient from "./ShopPageClient";
 
-import ShopClient from "../products/ShopClient";
-
-export default function ShopPage() {
-  return <ShopClient />;
+export default function Page() {
+  return <ShopPageClient />;
 }
