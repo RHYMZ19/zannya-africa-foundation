badda90351c49bb2f49ba71b46ec5076673d772a
Rahim <rahimssenabulya82@gmail.com>
2026-02-23 13:54:50 +0300

    Fix ca

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import Histories from "../Histories/Histories";
-import Leadership from "../Leadership/Leadership";
-import Locations from "../Locations/Locations";
+
 import Mission from "../Mission/Mission";
-import Partners from "../Partners/Partners";
 import styles from './AboutUs.module.css';
 
 export default function 
@@ -12,10 +9,6 @@ AboutUs() {
     return(
         <div className={styles.bun}>
             <h1 className={styles.headings}></h1>
-            <Histories></Histories> 
-            <Leadership></Leadership>
-            <Locations></Locations>
-            <Partners></Partners>
             <Mission></Mission>
         </div>
     )
