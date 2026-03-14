e767c4c73db552023a655e5e8324061b3ce599f3
Rahim <rahimssenabulya82@gmail.com>
2026-03-15 00:51:25 +0300

    Fix tftf

--- src/app/Programs/components/ProgramPage.tsx
+++ src/app/Programs/components/ProgramPage.tsx
@@ -210,9 +210,9 @@ export default function ProgramPage({
       </section>
 
       {/* PROGRAM OBJECTIVES */}
-      
+      <h2 className={styles.UectionTitle}>Program Objectives</h2>
 <section className={styles.highlights}>
-  <h2 className={styles.UectionTitle}>Program Objectives</h2>
+  
 
   <div className={styles.highlightsGrid}>
     {highlights.map((item, index) => (
