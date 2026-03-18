45c46106fde958b54e378abfb838e3cfaf72caa1
Rahim <rahimssenabulya82@gmail.com>
2026-03-18 18:53:26 +0300

    Fix ooooo

--- src/app/Missions/MissionsClient.tsx
+++ src/app/Missions/MissionsClient.tsx
@@ -61,9 +61,6 @@ export default function MissionsClient() {
       return () => unsubscribe();
     }, []);
 
-useEffect(()=>{
-fetchPosts()
-},[])
 
   useEffect(() => {
     const fetchLeaders = async () => {
