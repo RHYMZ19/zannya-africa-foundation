a89af8c950b7637ba22e3b04bf9226fa5464aabc
Rahim <rahimssenabulya82@gmail.com>
2026-03-29 00:14:27 +0300

    Fix tqqqq

--- src/app/Programs/climate-justice/page.tsx
+++ src/app/Programs/climate-justice/page.tsx
@@ -66,7 +66,7 @@ export default function ClimateJustice() {
     <ProgramPage
       title=" CLIMATE ACTION & ENVIRONMENT JUSTICE"
       description=""
-      image="https://res.cloudinary.com/dpwuym7xg/image/upload/v1771841037/zannya/uploads/images/ay22i17pmbutfxhjs9av.jpg"
+      image="https://res.cloudinary.com/dpwuym7xg/image/upload/v1774726306/zannya/uploads/images/bcff40usb7eiv1qk6qbb.jpg"
       intro="To Strengthen climate resilience and Environmental sustainability for 4,500 
       vulnerable Youths in Schools, Community and institutions in Uganda through Sports, 
       climate Advocacy and digital innovation by 2028."
