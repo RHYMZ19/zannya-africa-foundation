ca4384c617cbb34d1f8b4ce8c3520cc2e7be5c1a
Rahim Ssenabulya <rahimssenabulya82@gmail.com>
2026-07-06 22:30:45 +0300

    Fix drrr

--- src/app/Missions/MissionsClient.tsx
+++ src/app/Missions/MissionsClient.tsx
@@ -208,7 +208,7 @@ export default function MissionsClient() {
               Donate Now
             </button>
             <button className={styles.btnOutline}>
-              Get Involved
+              Get involved
             </button>
           </div>
         </div>
