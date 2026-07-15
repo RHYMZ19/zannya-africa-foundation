d2ce42326591a2e0de5de7b4133da9d39881ef37
Rahim Ssenabulya <rahimssenabulya82@gmail.com>
2026-07-15 11:50:41 +0300

    Fix ooo

--- src/app/Missions/MissionsClient.tsx
+++ src/app/Missions/MissionsClient.tsx
@@ -100,7 +100,7 @@ export default function MissionsClient() {
       
           <div className={styles.dropdownMenu}>
       
-            <a href="/articles" className={styles.dropdownItem}>
+            <a href="/weekly-newsletter" className={styles.dropdownItem}>
               📰 Articles
             </a>
       
