9212c5d693d54135dbf2e9735f2095383c5475f9
Rahim Ssenabulya <rahimssenabulya82@gmail.com>
2026-09-19 13:37:51 +0300

    Fix that

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -631,9 +631,8 @@ export default function MainPage() {
       <div className={styles.cardContent}>
         <h3>Upcoming Events</h3>
 
-        <a href="/events" className={styles.cardLink}>
-          <span>View More</span>
-          <div className={styles.arrowCircle}>→</div>
+        <a href="#/events" className={styles.cardLink}>
+          <span>No Events Yet!</span>
         </a>
       </div>
     </div>
